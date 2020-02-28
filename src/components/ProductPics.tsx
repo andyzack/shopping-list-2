626514de60dad5ccc2df80ee74e1308d334e9726
Andrew Zacharias <andyzack@gmail.com>
2020-02-28 14:22:42 +1100

    Add product images

---
+++ src/components/ProductPics.tsx
@@ -0,0 +1,46 @@
+import product1 from "../assets/images/product-1.jpg";
+import product2 from "../assets/images/product-2.jpg";
+import product3 from "../assets/images/product-3.jpg";
+import product4 from "../assets/images/product-4.jpg";
+import product5 from "../assets/images/product-5.jpg";
+import product6 from "../assets/images/product-6.jpg";
+import product7 from "../assets/images/product-7.jpg";
+import product8 from "../assets/images/product-8.jpg";
+
+const ProductPics =
+  [
+    {
+      "id": 0,
+      "productImage": product1
+    },
+    {
+      "id": 1,
+      "productImage": product2
+    },
+    {
+      "id": 2,
+      "productImage": product3
+    },
+    {
+      "id": 3,
+      "productImage": product4
+    },
+    {
+      "id": 4,
+      "productImage": product5
+    },
+    {
+      "id": 5,
+      "productImage": product6
+    },
+    {
+      "id": 6,
+      "productImage": product7
+    },
+    {
+      "id": 7,
+      "productImage": product8
+    }
+  ]
+
+export default ProductPics;
