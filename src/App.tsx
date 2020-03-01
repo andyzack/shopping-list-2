fef09f30eb43c09311e81bcade5636b5d906423d
Andrew Zacharias <andyzack@gmail.com>
2020-03-02 10:57:12 +1100

    Add dropdown menu to filter list

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 import useCustomFetch from './services/useCustomFetch';
 import ProductList from './components/ProductList';
 import ProductPics from './components/ProductPics';
+//import ProductMenu from './components/ProductMenu';
 
 // Type checking for title
 type AppProps = {
@@ -11,12 +12,32 @@ type AppProps = {
 
 function App(props: AppProps) {
 
-  // 1. Use custom hook to fetch data from API url
-  const url = 'https://api.jsonbin.io/b/5cae9a54fb42337645ebcad3';
+  // 1. Use custom hook to fetch data from API url https://api.jsonbin.io/b/5e58c93109ac43054813a160
+  const url = 'https://api.myjson.com/bins/nl8t8';
   const [data, loading, hasError] = useCustomFetch(url);
 
-  // 2. Get Product Pics List, then align to productImage using map func
+  // 2. Get Product Pics List, then align to productImage using map function
   const pics = Object.values(ProductPics);
+
+  // 3a. Use the dress selected state variable
+  const [dressSize, setDressSize] = useState('');
+
+  // 3b. Use the data selected state variable
+  let [dataSelected, setDataSelected] = useState([]);
+
+  // 3b. Use the data selected state variable
+  let dataSizes = Object.values(data) 
+  .map(item => item.size)
+  .reduce((prev, next) => [...prev, ...next], [])
+
+  dataSizes = dataSizes.filter((a:any, b:any) => dataSizes.indexOf(a) === b)
+
+  // 4. Helper function to handle menu change
+  function handleChange(val:any, data:any) {
+    setDressSize(val)
+    const dataFiltered = data.filter((item: { size: string | any[]; }) => item.size.includes(val));
+    setDataSelected(Object.values(dataFiltered));
+  }
 
   return (
     <>
@@ -25,24 +46,30 @@ function App(props: AppProps) {
           <h1 className="product-title">{props.title}</h1>
           <select
             className="product-filter"
+            value={dressSize}
+            onChange={(e) => {handleChange(e.target.value, data)}}
           >
-            <option key="1" value="XL">Size XL</option>
+            <option value="">Filter by size</option>
+            {
+              Object.values(dataSizes).map((item:string, index) => (
+                <option key={index} value={item}>Size {item}</option>
+              ))
+            }
           </select>
         </div>
         <div className={"product-container " + (loading? "out" : "in")}>
           <ul>
-          // Load Spinner while we fetch data
+
           {
             loading? (
               <li className="col-error"><div className="loader"></div></li>
             ) : ""
           }
 
-          // Load Product List if there is no errors
           {
             !hasError? (
-              Object.values(data).map((item, index) => (
-                <li key={item.index} className="product col-0">
+              Object.values(dataSelected.length? dataSelected : data).map((item, index) => (
+                <li key={item.index} className={"product col-"+dataSelected.length}>
                   <ProductList 
                     isSale={item.isSale}
                     isExclusive={item.isExclusive}
